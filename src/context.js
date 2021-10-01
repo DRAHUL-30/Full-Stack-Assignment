3e051914b14b6f0c427a861b826a0c80481009d2
DRAHUL <86136418+Rahulgithub07@users.noreply.github.com>
2021-10-01 14:52:48 +0530

    for login testing

--- src/context.js
+++ src/context.js
@@ -1,9 +1,34 @@
-import {createContext} from "react";
+import React from "react";
 
-let loggedIn=(false);
-export const context=createContext({loggedIn:loggedIn,changeLoggin});
-
-
-function changeLoggin(){
-    loggedIn=(true);
+class Abc extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: true,
+    };
+    this.changeState = this.changeState.bind(this);
+  }
+  changeState = () => {
+    this.setState({
+      login: !this.state.login,
+    });
+  };
+  render() {
+    const { login } = this.state;
+    return (
+        <>
+      <div className="App-body">
+        <button
+          type="button"
+          className={login ? "btn-primary" : "btn-danger"}
+          onClick={this.changeState}
+        >
+          {login ? " Login" : " Log Out"}
+        </button>
+      </div>
+      </>
+    );
+  }
 }
+
+export default Abc;
